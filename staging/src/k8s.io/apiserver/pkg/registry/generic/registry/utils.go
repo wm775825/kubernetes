c2f6b12cdf6b60e6ff48b43c33b08708614d3b46
wm775825 <1174929164@qq.com>
2023-12-03 01:30:06 +0800

    add Watch handler
    Signed-off-by: wm775825 <1174929164@qq.com>

--- staging/src/k8s.io/apiserver/pkg/registry/generic/registry/utils.go
+++ staging/src/k8s.io/apiserver/pkg/registry/generic/registry/utils.go
@@ -6,11 +6,13 @@ import (
 	"net/url"
 	"path"
 	"strings"
+	"sync"
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/conversion"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/apiserver/pkg/endpoints/request"
 
 	clusterv1alpha1 "github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1"
@@ -113,4 +115,109 @@ func unready(clusters []*clusterv1alpha1.Cluster) (skipFunc, sets.String) {
 
 func IsClusterReady(clusterStatus *clusterv1alpha1.ClusterStatus) bool {
 	return meta.IsStatusConditionTrue(clusterStatus.Conditions, clusterv1alpha1.ClusterConditionReady)
+}
+
+type singleDecoratedWatcher struct {
+	watcher   watch.Interface
+	decorator func(*watch.Event)
+}
+
+type watchMux struct {
+	lock    *sync.Mutex
+	sources []singleDecoratedWatcher
+	result  chan watch.Event
+	done    chan struct{}
+}
+
+func newWatchMux() *watchMux {
+	return &watchMux{
+		lock:   &sync.Mutex{},
+		result: make(chan watch.Event),
+		done:   make(chan struct{}),
+	}
+}
+
+var _ watch.Interface = &watchMux{}
+
+// AddSource shall be called before Start
+func (w *watchMux) AddSource(watcher watch.Interface, decorator func(*watch.Event)) {
+	w.sources = append(w.sources, singleDecoratedWatcher{
+		watcher:   watcher,
+		decorator: decorator,
+	})
+}
+
+// Start run all single watchers
+func (w *watchMux) Start() {
+	wg := &sync.WaitGroup{}
+	for i := range w.sources {
+		source := w.sources[i]
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			w.startSingle(source.watcher, source.decorator)
+		}()
+	}
+
+	go func() {
+		// close result chan after all goroutines exit, avoiding data race
+		defer close(w.result)
+		wg.Wait()
+	}()
+}
+
+func (w *watchMux) startSingle(source watch.Interface, decorator func(*watch.Event)) {
+	// if receiving the done signal from watchMux, stop this single watcher
+	defer source.Stop()
+	// if stream from the single watcher stopped, stop the watchMux to broadcast to all single watchers
+	defer w.Stop()
+
+	for {
+		var copyEvent watch.Event
+		select {
+		case event, ok := <-source.ResultChan():
+			if !ok {
+				return
+			}
+			copyEvent = *event.DeepCopy()
+			if decorator != nil {
+				decorator(&copyEvent)
+			}
+		case <-w.done:
+			return
+		}
+
+		// check whether watchMux is done before resend the event
+		// if done, stop resend events and return
+		select {
+		case <-w.done:
+			return
+		case w.result <- copyEvent:
+		}
+	}
+}
+
+func (w *watchMux) ResultChan() <-chan watch.Event {
+	return w.result
+}
+
+// Stop close the done channel to broadcast to all single watchers
+func (w *watchMux) Stop() {
+	// if done has been closed, return
+	select {
+	case <-w.done:
+		return
+	default:
+	}
+
+	// lock to avoid close channel more than once
+	w.lock.Lock()
+	defer w.lock.Unlock()
+	select {
+	case <-w.done:
+		return
+	default:
+		close(w.done)
+	}
 }
